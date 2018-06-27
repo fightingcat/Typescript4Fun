b93eb4e277f3ed0aed6a041965c9537772fd7f0a
fightingcat <fightingcat@users.noreply.github.com>
2018-06-27 14:11:04 +0800

    Update Tuple.ts
    命名错误

--- Tuple.ts
+++ Tuple.ts
@@ -2,4 +2,4 @@
 type Head<T extends any[]> = T extends [infer H, ...any[]] ? H : never;
 type Last<T extends any[]> = { 2: T[1], 1: T[0], 0: Last<Tail<T>> }[T extends [any, any] ? 2 : T extends [any] ? 1 : 0];
 type Tail<T extends any[]> = ((...x: T) => void) extends ((h: any, ...rest: infer R) => void) ? R : never;
-type Shift<T extends any[], V, U = (a: V, ...b: T) => void> = U extends (...x: infer R) => void ? R : never;
+type Unshift<T extends any[], V, U = (a: V, ...b: T) => void> = U extends (...x: infer R) => void ? R : never;
