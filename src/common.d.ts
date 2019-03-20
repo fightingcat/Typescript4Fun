36c33dd1a62296de5228ab76bc21692feb5f80a8
fightingcat <fightingcat@users.noreply.github.com>
2019-03-20 15:53:57 +0800

    Update common.d.ts

--- src/common.d.ts
+++ src/common.d.ts
@@ -1,3 +1,7 @@
 type TypeAssert<T, A> = T extends A ? T : never;
 
 type Overwrite<T, S extends any> = { [P in keyof T]: S[P] };
+
+type IsAny<T> = 1 extends (T extends never ? 1 : 0) ? 1 : 0;
+
+type IsNever<T> = [T] extends [never] ? 1 : 0;
