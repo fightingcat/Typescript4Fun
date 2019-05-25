a1c87e69dee507ed9f20b204573f89772db8149e
fightingcat <fightingcat@users.noreply.github.com>
2019-05-25 23:37:54 +0800

    Update tuple.d.ts

--- src/tuple.d.ts
+++ src/tuple.d.ts
@@ -7,3 +7,8 @@ type TupleTail<T extends any[]> = ((...t: T) => void) extends (x: any, ...t: inf
 type TupleUnshift<T extends any[], X> = ((x: X, ...t: T) => void) extends (...t: infer R) => void ? R : never;
 
 type TuplePush<T extends any[], X> = TypeAssert<Overwrite<TupleUnshift<T, any>, T & { [x: string]: X }>, any[]>;
+
+type TupleConcat<A extends any[], B extends any[]> = {
+    1: A,
+    0: TupleConcat<TuplePush<A, B[0]>, TupleTail<B>>
+}[B extends [] ? 1 : 0];
